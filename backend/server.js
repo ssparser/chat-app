c677b387768ef1c9b9ff2b26a66c1495af5f8c54
ksidharth <keesari64@gmail.com>
2024-06-16 20:20:27 +0530

    mysql

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const http = require("http")
-const mongoose = require('mongoose');
+const sequelize = require('./util/database');
+
+const authRoutes = require('./routes/authRoutes');
+
 require('dotenv').config();
+
 
 const PORT  = process.env.PORT || process.env.API_PORT
 
@@ -12,7 +16,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/auth', authRoutes);
+
 const server = http.createServer(app);
+
+sequelize.sync()
+.then(result => {
+    console.log(result);
+}).catch(err => {
+    console.error('Database sync error:', err);
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
